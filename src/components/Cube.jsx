92d214bec3312e454ca837848fb25e6396568bdb
Santiago <santiagovalencialeon@gmail.com>
2023-03-25 01:52:37 -0500

    traslate function working correctly with 2 dimensional object

---
+++ src/components/Cube.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { traslateFunc, scaleFunc } from '../helpers/arrayFunctions';
+
+const Cube = () => {
+   const [traslate, setTraslate] = useState({ x: 200, y: 50 });
+   const [scale, setScale] = useState({ x: 2, y: 2 });
+
+   const puntosInitial = [
+      [0, 0, 1],
+      [100, 0, 1],
+      [0, 100, 1],
+      [100, 100, 1],
+   ];
+
+   const [puntos, setPuntos] = useState(
+    //   traslateFunc(traslate.x, traslate.y, puntosInitial)
+        scaleFunc(scale.x, scale.y, puntosInitial)
+   );
+
+   return (
+      <>
+         {puntos.map((punto, index) => {
+            return (
+               <div
+                  key={index}
+                  style={{
+                     position: 'absolute',
+                     left: punto[0],
+                     top: punto[1],
+                     width: 10,
+                     height: 10,
+                     backgroundColor: 'red',
+                  }}></div>
+            );
+         })}
+      </>
+   );
+};
+
+export default Cube;
---
+++ src/helpers/arrayFunctions.js
@@ -0,0 +1,47 @@
+const multiplyArr = (tralationArray, vector) => {
+    let resultVector = [];
+    for (let i = 0; i < tralationArray.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < tralationArray[i].length; j++) {
+            sum += tralationArray[i][j] * vector[j];
+        }
+        resultVector.push(sum);
+    }
+    return resultVector;
+};
+
+
+const traslateFunc = (x, y, array) => {
+    let result = [];
+    const traslation = [
+        [1, 0, x],
+        [0, 1, y],
+        [0, 0, 1],
+    ];
+    //each array is a vector [x, y, 1]
+    array.forEach((v) => {
+        result.push(multiplyArr(traslation, v));
+    });
+    return result;
+};
+
+
+const scaleFunc = (x, y, vector) => {
+    const scale = [
+        [x, 0, 0],
+        [0, y, 0],
+        [0, 0, 1],
+    ];
+    return multiplyArr(scale, vector);
+};
+
+const rotateFunc = (angle, v) => {
+    const scale = [
+        [Math.cos(angle), -Math.sin(angle), 0],
+        [Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 1],
+    ];
+    return multiplyArr(scale, v);
+};
+
+export { traslateFunc, scaleFunc, rotateFunc };
